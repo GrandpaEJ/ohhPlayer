a28649b31f1e41123a7b12494945a025b07b19d7
Ebtisam Jubair <abirmridha2005@gmail.com>
2026-07-25 16:26:44 +0600

    feat: change time display from MM:SS to HH:MM:SS format

--- src/ui_state/time.rs
+++ src/ui_state/time.rs
@@ -1,9 +1,13 @@
 pub fn format_time(pos: f64, dur: f64) -> String {
+    let pos_s = pos as u32;
+    let dur_s = dur as u32;
     format!(
-        "{:02}:{:02} / {:02}:{:02}",
-        (pos as u32) / 60,
-        (pos as u32) % 60,
-        (dur as u32) / 60,
-        (dur as u32) % 60,
+        "{:02}:{:02}:{:02} / {:02}:{:02}:{:02}",
+        pos_s / 3600,
+        (pos_s % 3600) / 60,
+        pos_s % 60,
+        dur_s / 3600,
+        (dur_s % 3600) / 60,
+        dur_s % 60,
     )
 }
